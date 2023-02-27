c35b0da1d1eeaf09b919664b3311f80579870c91
King2oo9 <120119639+King2oo9@users.noreply.github.com>
2023-02-27 12:23:53 +0500

    Harakat static
    Harakat static

--- gazbenzin/gazbenzin/urls.py
+++ gazbenzin/gazbenzin/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .settings import DEBUG, MEDIA_URL,MEDIA_ROOT
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
 ]
 
-if DEBUG:
-    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
